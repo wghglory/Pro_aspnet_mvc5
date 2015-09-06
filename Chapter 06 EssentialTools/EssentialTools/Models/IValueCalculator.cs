92459fe8b4e4b99c5c88bb51b05d87e373c460ba
wghglory <wghglory89@gmail.com>
2015-09-06 11:27:12 -0400

    Pro asp.net mvc5
    Pro ASP.NET MVC 5 5th.edition

---
+++ Chapter 06 EssentialTools/EssentialTools/Models/IValueCalculator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace EssentialTools.Models
+{
+    public interface IValueCalculator
+    {
+
+        decimal ValueProducts(IEnumerable<Product> products);
+    }
+}
